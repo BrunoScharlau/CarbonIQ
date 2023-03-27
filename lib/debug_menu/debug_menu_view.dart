4326657a0944afd62bb5212dd6c0e889d20720b6
Carl von Bonin <carl.vonbonin@gmail.com>
2023-03-26 21:40:11 -0400

    Debug menu working

--- lib/debug_menu/debug_menu_view.dart
+++ lib/debug_menu/debug_menu_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gretapp/main_menu/main_menu_view.dart';
+import 'package:gretapp/registration/registration_view.dart';
+import 'package:gretapp/registration/user.dart';
 
 class DebugMenuView extends StatelessWidget {
   const DebugMenuView({super.key});
@@ -11,23 +14,54 @@ class DebugMenuView extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Start as new user'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Start as Greta (Environmental Activist)'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Start as Rick (Businessman)'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Start as Mike (Student)'),
-          )
+          const Padding(padding: EdgeInsets.only(top: 20)),
+          const SimulatedUserButton.newAccount(),
+          SimulatedUserButton(
+              UserAccount('Greta', []), 'Environmental Activist'),
+          SimulatedUserButton(UserAccount('Jeff', []), 'Businessman'),
+          SimulatedUserButton(UserAccount('Josh', []), 'Student'),
+          // TODO Actually populate the SurveySessions
         ],
+      ),
+    );
+  }
+}
+
+class SimulatedUserButton extends StatelessWidget {
+  final UserAccount? userAccount;
+  final String accountTypeDescriptor;
+  const SimulatedUserButton(this.userAccount, this.accountTypeDescriptor,
+      {super.key});
+
+  const SimulatedUserButton.newAccount({super.key})
+      : userAccount = null,
+        accountTypeDescriptor = 'New Account';
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: ElevatedButton(
+        onPressed: () {
+          if (userAccount == null) {
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const RegistrationView()),
+                (r) => false // Clear everything
+                );
+          } else {
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => MainMenuView(userAccount!)),
+                (r) => false // Clear everything
+                );
+          }
+        },
+        child: Text((userAccount != null)
+            ? 'Start as ${userAccount!.name} ($accountTypeDescriptor)'
+            : 'Start as new user'),
       ),
     );
   }
