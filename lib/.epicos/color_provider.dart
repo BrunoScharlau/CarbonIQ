983100cc63875f0aee651b43328bcebf66d7df49
Bruno Scharlau <bruno@scharlau.be>
2023-03-29 18:49:40 +0200

    added new files

---
+++ lib/.epicos/color_provider.dart
@@ -0,0 +1,22 @@
+import 'palette.json';
+
+class Palette{
+  Color primary, secondary, background, action;
+  Palette(this.primary, this.secondary, this.background, this.action)
+}
+
+static class ColorProvider {
+
+  ColorProvider(){
+  }
+
+  const int paletteCount{
+    return 
+  }
+
+  Color getColor(colorType type){
+
+  }
+
+  Enum colorType {primary, secondary, background, action}
+}
